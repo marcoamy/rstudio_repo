b157f667b2d2a6c095b089953aacda00c04a0bf6
= <marco-amy@live.com>
2021-01-13 14:38:20 +0100

    inital commit

---
+++ script.r
@@ -0,0 +1,7 @@
+# Programme testant si une année, saisie par l'utilisateur, est bissextile ou non
+AB <- function(annee) {
+  if (annee%%400 == 0 | (annee%%4==00 & annee%%100 != 0)) {
+    return(TRUE)
+  } else
+    return(FALSE)
+}
